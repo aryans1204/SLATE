ce82b39fe53068c192fb0f87cabc8b4baedc2150
Aryan Sharma <aryansharma@Aryans-MacBook-Air.local>
2022-09-29 21:37:47 +0800

    Higher level class for client and manifest functions

---
+++ configuration/client.h
@@ -0,0 +1,20 @@
+#include <vector>
+
+typedef struct FunctionDef {
+    //describes a function representation in the application manifest
+    void *(function)(double);
+    double p_objective;
+    int min_domain, max_domain;
+}
+
+class Client {
+    private:
+        vector<*FunctionDef> manifest;
+        int N;
+
+    public:
+        void createManifest();  //method to createManifest by taking input from client through stdin....the input stream can be modified other than stdin.
+
+        Client(int N); //class constructor for Client, takes in input N of the number of functions to be defined.
+
+}
